268f030b6224cbb6f5fcad6dd2877c0ddd840b29
Chris Banes <chris@banes.me>
2020-06-11 13:53:44 +0100

    Encrypt secret files using GPG

--- release/signing-setup.sh
+++ release/signing-setup.sh
@@ -1,16 +1,21 @@
-#!/bin/bash
+#!/bin/sh
+
+function decrypt {
+  PASSPHRASE=$1
+  INPUT=$2
+  OUTPUT=$3
+  gpg --batch --yes --passphrase $PASSPHRASE --output $OUTPUT --decrypt $INPUT
+}
 
 ENCRYPT_KEY=$1
 
 if [[ ! -z "$ENCRYPT_KEY" ]]; then
   # Decrypt Release key
-  openssl aes-256-cbc -md sha256 -d -in release/app-release.aes -out release/app-release.jks -k ${ENCRYPT_KEY}
-
+  decrypt ${ENCRYPT_KEY} release/app-release.gpg release/app-release.jks
   # Decrypt Play Store key
-  openssl aes-256-cbc -md sha256 -d -in release/play-account.aes -out release/play-account.json -k ${ENCRYPT_KEY}
-
+  decrypt ${ENCRYPT_KEY} release/play-account.gpg release/play-account.json
   # Decrypt Google Services key
-  openssl aes-256-cbc -md sha256 -d -in release/google-services.aes -out app/google-services.json -k ${ENCRYPT_KEY}
+  decrypt ${ENCRYPT_KEY} release/google-services.gpg app/google-services.json
 else
   echo "ENCRYPT_KEY is empty"
 fi
